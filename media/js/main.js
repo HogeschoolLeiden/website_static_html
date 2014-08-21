9ea48b2c24fdbb51a89f061c8d8050b3038bbf8e
Johan Post <johan@kees-tm.nl>
2014-08-21 15:03:05 +0200

    HTML/CSS overzicht template en detail template

---
+++ media/js/main.js
@@ -0,0 +1,18 @@
+
+$( document ).ready(function() {
+	
+	// dateCircleHeight ();
+// 	
+	// function dateCircleHeight () {
+		// var datepickerWidth = $(".ui-datepicker-calendar tr td span, .ui-datepicker-calendar tr td a").outerWidth();
+	  	// var datepickerHeight = datepickerWidth*0.75;
+// 	  
+	  	// $(".ui-datepicker-calendar tr td span, .ui-datepicker-calendar tr td a").height(datepickerHeight);
+	// }
+// 		
+	// $(window).resize(function() {
+		// dateCircleHeight ();
+	// });
+
+
+});
