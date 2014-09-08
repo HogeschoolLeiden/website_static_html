05afd6f5dfa6cccc16ac00f4b94f999caf757852
Johan Post <johan@kees-tm.nl>
2014-09-08 16:28:01 +0200

    Vind je studie (find your study) finished, added widgets and elements to pages

--- www/media/js/main.js
+++ www/media/js/main.js
@@ -1,6 +1,11 @@
 
 $( document ).ready(function() {
 	
+	$(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) { 		event.preventDefault();
+		$(this).ekkoLightbox(); 
+	});
+
+	
 	// dateCircleHeight ();
 // 	
 	// function dateCircleHeight () {
