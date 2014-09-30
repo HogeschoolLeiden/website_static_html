9aa362db99ca6be5fe9e8cc2c1adc36366924ec9
Johan Post <johan@kees-tm.nl>
2014-09-30 10:42:12 +0200

    Added Homepage (home.html) and all widgets (detail.html). Changes to header and footer.

--- www/media/js/main.js
+++ www/media/js/main.js
@@ -1,36 +1,92 @@
+var doc = document.documentElement;
+doc.setAttribute('data-useragent', navigator.userAgent);
 
 $( document ).ready(function() {
 	
+	/* SWIPER SLIDER */	
+	if ($(".swiper-container").length > 0){
+	 $(window).bind('orientationchange', function(event) {
+	 	window.location.reload(false);
+	 });
+	}
+	
+	if ($(".swiper-container").length > 0){
+		
+		var mySwiper = $('.swiper-container').swiper({
+		    resizeReInit: true,
+		    mode:'horizontal',
+		    loop: true,
+		    simulateTouch: true,
+		    centeredSlides: true,
+		    pagination: ".slider-pagination",
+		    createPagination: true,
+		    paginationClickable: true,
+		    progress:true,
+		    onProgressChange: function(swiper){
+		        for (var i = 0; i < swiper.slides.length; i++){
+		          var slide = swiper.slides[i];
+		          var progress = slide.progress;
+		          var translate = progress*swiper.width;  
+		          var opacity = 1 - Math.min(Math.abs(progress),1);
+		          slide.style.opacity = opacity;
+		          swiper.setTransform(slide,'translate3d('+translate+'px,0,0)');
+		     	}
+		    },
+		    onTouchStart:function(swiper){
+		    for (var i = 0; i < swiper.slides.length; i++){
+		      swiper.setTransition(swiper.slides[i], 0);
+		    }
+		  },
+		  onSetWrapperTransition: function(swiper, speed) {
+		    for (var i = 0; i < swiper.slides.length; i++){
+		      swiper.setTransition(swiper.slides[i], speed);
+		    }
+		  }
+		  });
+		  
+	    $('.swipenav.prev').on('click', function(e){
+			e.preventDefault();
+			mySwiper.swipePrev();
+		});
+		
+		$('.swipenav.next').on('click', function(e){
+			e.preventDefault();
+			mySwiper.swipeNext();
+		}) ;	
+	
+	}
+	/* // SWIPER SLIDER */	
+	
+	/* lightbox */
 	$(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) { 		event.preventDefault();
 		$(this).ekkoLightbox(); 
 	});
+	/* // lightbox */
 
-	$(".validate").validate({
-		errorElement:'span',
-		rules: {
-			email: {
-				required:true,
-				email:true
+	/* form validation */
+	if ($(".validate").length > 0){
+		$(".validate").validate({
+			errorElement:'span',
+			rules: {
+				email: {
+					required:true,
+					email:true
+				}
 			}
-		}
-	});
-	jQuery.extend(jQuery.validator.messages, {
-	    required: "Verplicht",
-	    email: "Incorrect"
-	});
+		});
+		jQuery.extend(jQuery.validator.messages, {
+		    required: "Verplicht",
+		    email: "Incorrect"
+		});
+	}
+	/* // form validation */
 	
-	// dateCircleHeight ();
-// 	
-	// function dateCircleHeight () {
-		// var datepickerWidth = $(".ui-datepicker-calendar tr td span, .ui-datepicker-calendar tr td a").outerWidth();
-	  	// var datepickerHeight = datepickerWidth*0.75;
-// 	  
-	  	// $(".ui-datepicker-calendar tr td span, .ui-datepicker-calendar tr td a").height(datepickerHeight);
-	// }
-// 		
-	// $(window).resize(function() {
-		// dateCircleHeight ();
-	// });
-
+	/* open share in new window */
+	$('.share a').click(function(){
+		window.open($(this).attr('href'),'title', 'width=800, height=700');
+        return false;
+	});
+	/* // open share in new window */
+	
 
 });
